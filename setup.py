386c759455ce652b193620136bd67311939a1526
Clemens Eppner <ceppner@nvidia.com>
2024-11-19 11:02:41 -0800

    Version v1.10.0

--- setup.py
+++ setup.py
@@ -15,16 +15,6 @@
 # * https://setuptools.pypa.io/en/latest/setuptools.html#setup-cfg-only-projects
 
 # Third Party
-from setuptools import setup
+import setuptools
 
-if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+setuptools.setup()
